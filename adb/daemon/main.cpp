00905ff9cf7acc550015a008f9513546e36af52a
Pierre-Hugues Husson <phh@phh.me>
2017-11-22 14:14:19 +0100

    [UGLY] Disable ADB auth
    Disable ADB auth (I can't find how to set ro.adb.secure)
    
    Change-Id: I8b1e440e18f0f14da01d14b72db396ab43ba4028

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -158,9 +158,7 @@ int adbd_main(int server_port) {
     // descriptor will always be open.
     adbd_cloexec_auth_socket();
 
-    if (ALLOW_ADBD_NO_AUTH && !android::base::GetBoolProperty("ro.adb.secure", false)) {
         auth_required = false;
-    }
 
     adbd_auth_init();
 
